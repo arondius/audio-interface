38fe0180f82f6fc05c7b2deb1832103ba9b07162
Arend Hosman <info@webbb-online.com>
2020-03-27 12:28:54 +0100

    Adds VolumeDial

---
+++ src/components/VolumeDial/VolumeDial.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './VolumeDial.scss';
+
+const VolumeDial = () => {
+  return (
+    <div class="volume">
+      <div class="volume-dial">
+          <div class="volume-dial-led"></div>
+          <div class="led volume-led-1 led--on"></div>
+          <div class="led volume-led-2 led--on"></div>
+          <div class="led volume-led-3 led--on"></div>
+          <div class="led volume-led-4 led--on"></div>
+          <div class="led volume-led-5 led--on"></div>
+          <div class="led volume-led-6 led--on"></div>
+          <div class="led volume-led-7 led--on"></div>
+          <div class="led volume-led-8 led--on"></div>
+          <div class="led volume-led-9 led--on"></div>
+          <div class="led volume-led-10 led--on"></div>
+          <div class="led volume-led-11 led--off"></div>
+          <div class="led volume-led-12 led--off"></div>
+          <div class="led volume-led-13 led--off"></div>
+          <div class="led volume-led-14 led--off"></div>
+          <div class="led volume-led-15 led--off"></div>
+          <div class="led volume-led-16 led--off"></div>
+          <div class="led volume-led-17 led--off"></div>
+          <div class="led volume-led-18 led--off"></div>
+          <div class="led volume-led-19 led--off"></div>
+        </div>
+    </div>
+  )
+}
+
+export default VolumeDial;
