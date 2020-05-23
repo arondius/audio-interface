09d59015f3f9c6be8c71127c0d64f69327870e84
Arend Hosman <info@webbb-online.com>
2020-05-23 21:10:11 +0200

    Adds a basic EventListener to change volume

--- src/components/VolumeDial/VolumeDial.js
+++ src/components/VolumeDial/VolumeDial.js
@@ -5,14 +5,34 @@ import MiniLedList from '../MiniLedList/MiniLedList';
 
 const VolumeDial = (props) => {
   console.log('props', props);
-  const { volume, volumeSteps } = props;
-  const changeVolume = ($event) => {
-    console.log('event', $event);
+  let { volume, volumeSteps } = props;
+  const onVolumeChange = ($event) => {
+
+    console.log('$event.target', $event.target);
+    const y = $event.clientY;
+    const modifier = 500;
+
+    const changeVolume = (e) => {
+      if(e.clientY > y) {
+        volume -= (e.clientY / modifier)
+        if (volume < 0) volume = 0;
+      } else {
+        volume += (e.clientY / modifier)
+        if (volume > 100) volume = 100;
+      }
+      return volume;
+    }
+
+    $event.target.addEventListener('mousemove', changeVolume);
+
+    $event.target.addEventListener('mouseup', (e) => {
+      e.target.removeEventListener('mousemove', changeVolume);
+    });
   }
 
   return (
     <div className="volume">
-      <div className="volume-dial" onMouseDown={(e) => changeVolume(e)}>
+      <div className="volume-dial" onMouseDown={(e) => onVolumeChange(e)}>
           <VolumeDialLed volume={volume} />
           <MiniLedList volume={volume} volumeSteps={volumeSteps}/>
         </div>
